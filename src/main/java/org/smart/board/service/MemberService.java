afccb404a41b4dad3053633d13d10695d4197ad3
Hong <hongsyai@gmail.com>
2022-02-21 15:14:35 +0900

    회원가입_로그인href, ajax(DB연결)

--- src/main/java/org/smart/board/service/MemberService.java
+++ src/main/java/org/smart/board/service/MemberService.java
@@ -5,4 +5,7 @@ import org.smart.board.entity.Member;
 public interface MemberService {
     // 회원가입
     public int insertMember(Member member);
+
+    //회원조회 return null인 경우만 사용 가능
+    public Member findOne(String usrid);
 }
